8809ac14bcf1713a05ec7d71f8042cb46e94e124
許峯僑 <51364627+skylight0306@users.noreply.github.com>
2021-09-02 18:51:31 +0800

    Create Factorial Trailing Zeroes.cpp

---
+++ 172. Factorial Trailing Zeroes/Factorial Trailing Zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int num = 0;
+        while ( n > 0 )
+        {
+            n /= 5 ;
+            num += n ;
+        }
+        
+        return num ;
+    }
+};
